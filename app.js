39e14cff6787015d28ce9bc8270a6a493c5d9e29
shaunmillar <shaunmillaris@gmail.com>
2019-01-18 10:14:45 -0800

    Update app.js
    added support for OpenShift V3

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+// openshift v2
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
+// openshift v3
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
 app.listen(server_port, server_ip_address, function () {
   console.log('Example app listening on port ', server_port);
 });
