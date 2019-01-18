60a1dd2e56933745008a740b886197648a99457c
shaunmillar <shaunmillaris@gmail.com>
2019-01-18 10:22:36 -0800

    Update app.js
    minor wording update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World from OpenShift!');
 });
 
 app.listen(server_port, server_ip_address, function () {
